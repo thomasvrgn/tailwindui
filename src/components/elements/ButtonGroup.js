ab1e3f645238f6c1537b59675ba9858688101fa7
NessMC <contact@nessmc.fr>
2021-05-02 12:21:51 +0200

    ✨ Basic button group component added.

---
+++ src/components/elements/ButtonGroup.js
@@ -0,0 +1,11 @@
+import { Apply } from '../../utils/ApplyChildren'; 
+
+export const Basic = ({ children, className, apply }) => {
+  return (
+    <div className={'flex flex-row divide-x divide-gray-300 text-center w-max border-gray-300 border rounded-md shadow-sm ' + className}>
+      <Apply props={apply} className="px-4 py-1 text-gray-600 focus:outline-none font-medium focus:bg-gray-50">
+        {children}
+      </Apply>
+    </div>
+  )
+}
